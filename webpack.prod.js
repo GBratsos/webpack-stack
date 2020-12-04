a6692c8fb31e4c2f88663c292024dd0804e6e132
George Bratsos <geobratsos@gmail.com>
2020-12-04 15:25:20 +0200

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,60 +1,69 @@
 const path = require('path');
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-//const { VueLoaderPlugin } = require('vue-loader');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
 
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
-  optimization: {
-    minimize: true,
-    minimizer: [
-    new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false,
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-        },
-        extractComments: false,
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
     }),
-    new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.bundle.js'
-  },
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src/img'),
+          to: path.resolve(__dirname, 'dist/img')
+        },
+      ],
+    }),
+
+    new ImageMinimizerPlugin({
+      cache: false,
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+
   module: {
     rules: [
       {
-        test: /\.(sa|sc|c)ss$/,
+        test: /\.(scss|css)$/,
         use: [
+          MiniCssExtractPlugin.loader,
           {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          {
-            loader: "postcss-loader",
+            loader: 'css-loader',
             options: {
-              autoprefixer: {
-                browsers: ['last 2 versions', 'ie >= 9']
-              },
+              importLoaders: 2,
               sourceMap: false,
-              plugins: () => [
-                autoprefixer
-              ]
             },
           },
-          'sass-loader'
-        ]
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
+      
+
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [{
@@ -64,19 +73,17 @@ module.exports = {
           }
         }]
       },
+
+
       {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'img/',
-              useRelativePath: true,
-            }
-          },
-        ]
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+        test: /\.(jpe?g|png|gif)$/i,
       },
+
+
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
@@ -84,26 +91,29 @@ module.exports = {
           loader: 'babel-loader'
         }
       },
-      // {
-      //   test: /\.vue$/,
-      //   loader: 'vue-loader'
-      // },
-    ],
+    ]
   },
-  plugins: [
-    // new webpack.ProvidePlugin({
-    //   $: "jquery",
-    //   jQuery: "jquery"
-    // }),
-    //new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ],
-  resolve: {
-    alias: {
-      //vue: 'vue/dist/vue.js',
-      //jquery: 'jquery/dist/jquery.js'
-    }
-  }
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        test: /\.js(\?.*)?$/i,
+        parallel: true,
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin()
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+    publicPath: ''
+  },
 };
